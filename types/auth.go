ef9d4b3e653f641df2be7452aaec8544e075f608
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-05-22 11:08:27 +0530

    add: RegisterUser request body struct

--- types/auth.go
+++ types/auth.go
@@ -1,66 +1,72 @@
 package types
 
 import (
+	"fmt"
+	"net/http"
 	"strings"
 
-	"github.com/IAmRiteshKoushik/pulse/pkg"
 	v "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
-/*
-* Request and response for login request. During the response, the cookies are
-* setup as well for further authentication requests
- */
-type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+type RegisterUserRequest struct {
+	Email      string `json:"email"`
+	GhUsername string `json:"github_username"`
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	LastName   string `json:"last_name"`
 }
 
-func (r *LoginRequest) Validate() error {
-	// Trim space
+func (r *RegisterUserRequest) Validate() error {
 	r.Email = strings.TrimSpace(r.Email)
-	r.Password = strings.TrimSpace(r.Password)
+	r.GhUsername = strings.TrimSpace(r.GhUsername)
+	r.FirstName = strings.TrimSpace(r.FirstName)
+	r.MiddleName = strings.TrimSpace(r.MiddleName)
+	r.LastName = strings.TrimSpace(r.LastName)
 
-	return v.ValidateStruct(r,
-		v.Field(r.Email, v.Required, is.Email),
-		v.Field(r.Password,
-			v.Required,
-			v.Length(8, 50),
-			v.Match(pkg.Rex.PasswordRegex),
-		),
+	err := v.ValidateStruct(r,
+		v.Field(&r.Email, v.Required, is.EmailFormat),
+		v.Field(&r.GhUsername, v.Required, v.Length(3, 50)),
+		v.Field(&r.FirstName, v.Required, v.Length(2, 50), is.Alpha),
+		v.Field(&r.MiddleName, v.Required, v.Length(2, 50), is.Alpha),
+		v.Field(&r.LastName, v.Required, v.Length(2, 50), is.Alpha),
 	)
+	if err != nil {
+		return err
+	}
+
+	// Check for Valid GitHub username
+	url := fmt.Sprintf("https://api.github.com/users/%s", r.GhUsername)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return nil
+	case http.StatusNotFound:
+		return fmt.Errorf("Invalid GitHub username")
+	default:
+		return fmt.Errorf("Could not search for GitHub username")
+	}
 }
 
-// TODO: Need to send more data
-type LoginResponse struct {
-	Message   string `json:"message"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+type RegisterUserOtpVerifyRequest struct {
+	Email string `json:"email"`
+	Otp   string `json:"otp"`
 }
 
-/*
-* Request and response for customer registrations. During the response, a
-* 5-minute valid token is setup which is added in the cookie jar. And this can
-* be used to further add in client-side validations if the OTP is being
-* entered really late!
- */
-type CustomerRegisterRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func (r *CustomerRegisterRequest) Validate() error {
+func (r *RegisterUserOtpVerifyRequest) Validate() error {
 	r.Email = strings.TrimSpace(r.Email)
-	r.Password = strings.TrimSpace(r.Password)
+	r.Otp = strings.TrimSpace(r.Otp)
 
 	return v.ValidateStruct(r,
-		v.Field(r.Email, v.Required, is.Email),
-		v.Field(r.Password,
-			v.Required,
-			v.Length(9, 50),
-			v.Match(pkg.Rex.PasswordRegex),
-		),
+		v.Field(&r.Email, v.Required, is.EmailFormat),
+		v.Field(&r.Otp, v.Required, v.Length(6, 6), is.Digit),
 	)
 }
