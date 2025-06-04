cb76ae1ea38904ead30fe03abe24f3f1a79f46db
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-06-04 20:51:41 +0530

    add: Placeholder query during registration-process with no leaderboard

---
+++ db/queries/misc.sql
@@ -0,0 +1,12 @@
+-- name: FetchParticipantListQuery :many
+SELECT
+  CASE
+    WHEN middle_name IS NULL THEN first_name || ' ' || last_name
+    ELSE first_name || ' ' || middle_name || ' ' || last_name
+  END as full_name,
+  0 as bounty,
+  0 as solutions
+FROM
+  user_account
+WHERE
+  status = true;
