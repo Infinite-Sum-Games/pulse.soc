18f8846dc9b583ba1ded128b4a6b53cccf9a4434
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-06-05 22:34:37 +0530

    add: FetchAllRegistrations query

--- controllers/leaderboard.go
+++ controllers/leaderboard.go
@@ -1,12 +1,44 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/IAmRiteshKoushik/pulse/cmd"
+	db "github.com/IAmRiteshKoushik/pulse/db/gen"
+	"github.com/IAmRiteshKoushik/pulse/pkg"
 	"github.com/gin-gonic/gin"
 )
+
+func FetchRegistrationBoard(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	conn, err := cmd.DBPool.Acquire(ctx)
+	if err != nil {
+		pkg.DbError(c, err)
+		return
+	}
+	defer conn.Release()
+	q := db.New()
+
+	profiles, err := q.FetchParticipantListQuery(ctx, conn)
+	if err != nil {
+		pkg.DbError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "All registered participants fetched successfully",
+		"profiles": profiles,
+	})
+	cmd.Log.Info(fmt.Sprintf(
+		"[SUCCESS]: Processed request at %s %s",
+		c.Request.Method, c.FullPath(),
+	))
+}
 
 func FetchLeaderboard(c *gin.Context) {
 
@@ -17,5 +49,4 @@ func FetchLeaderboard(c *gin.Context) {
 		"[SUCCESS]: Processed request at %s %s",
 		c.Request.Method, c.FullPath(),
 	))
-	return
 }
