56fe85db35b1e1a28dece611411e3c703e415d1e
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-06-06 10:46:10 +0530

    fix: Send github username in registration board

--- db/queries/misc.sql
+++ db/queries/misc.sql
@@ -4,6 +4,7 @@ SELECT
     WHEN middle_name IS NULL THEN first_name || ' ' || last_name
     ELSE first_name || ' ' || middle_name || ' ' || last_name
   END as full_name,
+  ghUsername as github_username,
   0 as bounty,
   0 as solutions
 FROM
