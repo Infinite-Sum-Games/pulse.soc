42276911be2a38e7533a0dd250e838a339b92f87
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2025-12-07 02:53:19 +0530

    ref: token handling to use CustomClaims struct

--- pkg/token.go
+++ pkg/token.go
@@ -7,8 +7,14 @@ import (
 	"github.com/IAmRiteshKoushik/pulse/cmd"
 	"github.com/golang-jwt/jwt/v5"
 )
+type CustomClaims struct {
+	Email      string `json:"email"`
+	GhUsername string `json:"github_username"`
+	TokenType  string `json:"token_type"`
+	jwt.RegisteredClaims
+}
 
-func CreateToken(ghUsername, email, tokenType string) (string, error) {
+func CreateToken(GhUsername, email, tokenType string) (string, error) {
 	var expiryAt time.Time
 	switch tokenType {
 	case "temp_token":
@@ -22,16 +28,18 @@ func CreateToken(ghUsername, email, tokenType string) (string, error) {
 			"temp_token", "access_token", "refresh_token")
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
-		jwt.RegisteredClaims{
-			ID:        email,
-			Audience:  []string{ghUsername},
-			Issuer:    "api.season-of-code",
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
+	claims := CustomClaims{
+		Email:      email,
+		GhUsername: GhUsername,
+		TokenType:  tokenType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:   "api.season-of-code",
 			ExpiresAt: jwt.NewNumericDate(expiryAt),
-			Subject:   tokenType,
-		})
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
 
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString([]byte(cmd.AppConfig.JWTSecret))
 	if err != nil {
 		return "", err
@@ -39,8 +47,8 @@ func CreateToken(ghUsername, email, tokenType string) (string, error) {
 	return tokenString, nil
 }
 
-func VerifyToken(tokenString string) (*jwt.RegisteredClaims, error) {
-	claims := &jwt.RegisteredClaims{}
+func VerifyToken(tokenString string) (*CustomClaims, error) {
+	claims := &CustomClaims{}
 	token, err := jwt.ParseWithClaims(
 		tokenString,
 		claims,
@@ -58,7 +66,7 @@ func VerifyToken(tokenString string) (*jwt.RegisteredClaims, error) {
 	if !token.Valid {
 		return nil, fmt.Errorf("invalid token")
 	}
-	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok {
+	if claims, ok := token.Claims.(*CustomClaims); ok {
 		if claims.ExpiresAt != nil && claims.ExpiresAt.Before(time.Now()) {
 			return nil, fmt.Errorf("token expired")
 		}
